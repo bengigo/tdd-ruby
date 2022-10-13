54408304e90a16b67f071b06930a4be94cdcd56b
a-laarabi <contact.laarabi@gmail.com>
2022-10-13 16:07:55 +0100

    Create testing fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -23,8 +23,32 @@ describe Solver do
     it 'should reverse a given string' do
       expect(@solver.reverse('hello')).to eq('olleh')
     end
+
+    it 'should reverse 123456 to 654321' do
+      expect(@solver.reverse('123456')). to eq('654321')
+    end
+
+    it 'should reverse 12_34-56 to 65-43_21' do
+      expect(@solver.reverse('12_34-56')). to eq('65-43_21')
+    end
   end
 
-  
+  context 'testing fizzbuzz method' do
+    it 'should return "fizz" if the parameter is divisible by 3' do
+      expect(@solver.fizzbuzz(6)).to eq('fizz')
+    end
+
+    it 'should return "buzz" if the parameter is divisible by 5' do
+      expect(@solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'should return "fizzbuzz" if the parameter is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'should return the number if the parameter is not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(4)).to eq('4')
+    end
   end
+
 end
