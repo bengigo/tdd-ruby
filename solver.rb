2432142cf42e0fb77e635eec0aec8f2abc7f5200
bengigenc <genc.bengi@gmail.com>
2022-10-13 18:24:07 +0300

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,4 +1,14 @@
 class Solver
+  def factorial(num)
+    if num.zero?
+      1
+    elsif num.negative?
+      raise 'Negative number entered'
+    else
+      num * factorial(num - 1)
+    end
+  end
+
 
   def fizzbuzz(num)
     if (num % 3).zero? and (num % 5).zero?
